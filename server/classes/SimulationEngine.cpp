696c3751e83c6455084ccbb2a521d16204e108c5
Terézia Vrábeľová <vrabelova2@stud.uniza.sk>
2026-02-09 21:30:57 +0100

    - feat: simulation engine .cpp

---
+++ server/classes/SimulationEngine.cpp
@@ -0,0 +1,41 @@
+#include "SimulationEngine.h"
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+SimulationEngine::SimulationEngine()
+{
+
+}
+
+SimulationEngine::~SimulationEngine()
+{
+
+}
+
+void SimulationEngine::addWorker(Worker *newWorker)
+{
+    this->workers.push_back(newWorker);
+}
+
+void SimulationEngine::removeWorker(int workerId)
+{
+    for(int i = 0; i < this->workers.size(); i++) {
+        Worker* worker = this->workers[i];
+        if(worker->getId() == workerId) {
+            delete worker;
+            this->workers.erase(this->workers.begin() + i);
+            break;
+        }
+    }
+}
+
+void SimulationEngine::printWorkers()
+{
+    for (int i = 0; i < this->workers.size(); i++)
+    {
+        Worker* worker = this->workers[i];
+        cout << "["<< worker->getRole() << ", " << worker->getId() << "] " << worker->getName() << endl;
+    }
+    
+}
